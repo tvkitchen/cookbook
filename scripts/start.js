09c85e7571b41a6095b3e323394d178816d2557b
Dan Schultz <slifty@gmail.com>
2020-08-27 15:27:59 -0400

    Set up initial structure
    This sets up the baseline repository structure, including the various
    dev tools we'll be using (e.g. linters, babel, etc.)
    
    There are also scripts that will make it easy to spin up and tear down
    a local kafka instance for folks who don't have one.
    
    Issue #1

---
+++ scripts/start.js
@@ -0,0 +1,36 @@
+import assert from 'assert'
+import path from 'path'
+import fs from 'fs'
+import { execSync } from 'child_process'
+
+if (process.argv.length !== 3) {
+	console.log(
+		'\x1b[35mERROR:\x1b[0m',
+		'You must pass a recipe name.',
+	)
+	console.log(
+		'\x1b[35mERROR:\x1b[0m',
+		'yarn start {recipe-name}',
+	)
+	process.exit()
+}
+
+const recipe = process.argv[2]
+const recipePath = path.join(
+	'recipes/',
+	recipe,
+	'src/index.js',
+)
+
+if(!fs.existsSync(recipePath)) {
+	console.log(
+		'\x1b[35mERROR:\x1b[0m',
+		`'${recipePath}' does not exist.`,
+	)
+	process.exit()
+}
+
+execSync(
+	`yarn babel-node ${recipePath}`,
+	{ stdio: 'inherit' },
+)
