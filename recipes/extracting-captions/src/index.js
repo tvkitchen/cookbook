76296278b95ff771a3afc879b5427e99d260168b
Dan Schultz <slifty@gmail.com>
2020-09-09 14:44:49 -0400

    Create ccextractor recipe
    This creates the first recipe, which shows how a person could use TV
    Kitchen to extract captions from a video file that has a caption stream.
    
    Issue #1

--- recipes/extracting-captions/src/index.js
+++ recipes/extracting-captions/src/index.js
@@ -1,10 +1,34 @@
+import path from 'path'
+import {
+	applianceEvents,
+	dataTypes,
+} from '@tvkitchen/base-constants'
 import { Countertop } from '@tvkitchen/countertop'
-import { VideoFileIngestionAppliance } from '@tvkitchen/appliance-video-file-ingestion'
-import { CcExtractorAppliance } from '@tvkitchen/appliance-ccextractor'
+import VideoFileIngestionAppliance from '@tvkitchen/appliance-video-file-ingestion'
+import CcExtractorAppliance from '@tvkitchen/appliance-ccextractor'
 
-const countertop = new Countertop()
+// Create the Countertop
+const countertop = new Countertop({
+	kafkaSettings: {
+		brokers: ['127.0.0.1:9092']
+	}
+})
+
+// Add the Appliances
 countertop.addAppliance(VideoFileIngestionAppliance, {
-	filePath: ''
+	filePath: path.join(__dirname, '../data/sample.ts'),
 })
 countertop.addAppliance(CcExtractorAppliance)
+
+// Register a Payload event handler
+countertop.on(
+	applianceEvents.PAYLOAD,
+	(payload) => {
+		if (payload.type === dataTypes.TEXT.ATOM) {
+			console.log(payload)
+		}
+	},
+)
+
+// Start processing
 countertop.start()
